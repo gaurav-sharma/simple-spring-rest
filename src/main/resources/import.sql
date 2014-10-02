c8a1e498ca3376c274448e9bd117ee3a4c7edd0c
Gaurav Sharma <sharmanaetor@gmail.com>
2014-10-01 22:40:37 -0500

    initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into person(id, first_name, last_name) values(1, 'George', 'Carlin');
+insert into person(id, first_name, last_name) values(2, 'Richard', 'Pryor');
+insert into person(id, first_name, last_name) values(3, 'Dave', 'Chappelle');
+insert into person(id, first_name, last_name) values(4, 'Louis', 'C.K.');
+insert into person(id, first_name, last_name) values(5, 'Eddie', 'Murphy');
+insert into person(id, first_name, last_name) values(6, 'Chris', 'Rock');
+insert into person(id, first_name, last_name) values(7, 'Robin', 'Williams');
